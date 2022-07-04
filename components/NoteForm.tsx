75f95f99095621c0f8df6bcbcbd782b81e017bcc
hiroto-hara <xre0605@gmail.com>
2022-07-04 21:30:56 +0900

    api-revalidate

---
+++ components/NoteForm.tsx
@@ -0,0 +1,8 @@
+import { FormEvent, FC } from 'react'
+import { supabase } from '../utils/supabase'
+import useStore from '../store'
+import { Spinner } from './Spinner'
+
+export const NoteForm: FC = () => {
+  return <div></div>
+}
